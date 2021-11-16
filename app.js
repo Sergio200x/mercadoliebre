3244c7a7ec9d47116073e25d3bd6d58bf4b92fc1
Sergio200x <sergio_200x@hotmail.com>
2021-11-16 12:25:26 -0300

    agregando forms

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const { dirname } = require("path");
 const app = express();
 const path = require('path')
 const port =3030
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.get("/", (res, resp)=> resp.sendFile(path.join(__dirname,"/views/home.html")))
+app.get("/register.html", (res, resp)=> resp.sendFile(path.join(__dirname,"/views/register.html")))
+app.get("/login.html", (res, resp)=> resp.sendFile(path.join(__dirname,"/views/login.html")))
 
 app.listen(port,()=>{console.log("servidor activo")})
+
